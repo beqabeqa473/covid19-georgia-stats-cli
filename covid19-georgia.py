8efc1df524a13b15b79f224fc70b286aed133802
Beqa Gozalishvili <beqaprogger@gmail.com>
2021-01-09 08:52:16 +0400

    Adapted to current website layout

--- covid19-georgia.py
+++ covid19-georgia.py
@@ -14,8 +14,11 @@ totalRecovered = statsItems[1].find_all("span", class_="quantity-numver")[0].tex
 lastRecovered = statsItems[1].find_all("span", class_="quantity-numver")[1].text.rstrip()
 totalDeaths = statsItems[2].find_all("span", class_="quantity-numver")[0].text.rstrip()
 lastDeaths = statsItems[2].find_all("span", class_="quantity-numver")[1].text.rstrip()
-quarantine = statsItems[3].find_all("span", class_="quantity-numver")[0].text.rstrip()
-hospitalize = statsItems[4].find_all("span", class_="quantity-numver")[0].text.rstrip()
-covidHotels = statsItems[5].find_all("span", class_="quantity-numver")[0].text.rstrip()
-text = f"total cases: {totalCases} (In last 24 hours {lastCases});\ntotal Recovered: {totalRecovered} (In last 24 hours {lastRecovered});\ntotal deaths: {totalDeaths} (In last 24 hours {lastDeaths});\nquarantined: {quarantine};\nHospitalized: {hospitalize};\nIn Covid hotels: {covidHotels}"
+lastTests = statsItems[3].find_all("span", class_="quantity-numver")[0].text.rstrip()
+antigenTests = statsItems[3].find_all("span", class_="quantity-numver")[1].text.rstrip()
+pcrTests = statsItems[3].find_all("span", class_="quantity-numver")[2].text.rstrip()
+quarantine = statsItems[4].find_all("span", class_="quantity-numver")[0].text.rstrip()
+hospitalize = statsItems[5].find_all("span", class_="quantity-numver")[0].text.rstrip()
+clinicHotels = statsItems[6].find_all("span", class_="quantity-numver")[0].text.rstrip()
+text = f"total cases: {totalCases} (In last 24 hours {lastCases});\ntotal Recovered: {totalRecovered} (In last 24 hours {lastRecovered});\ntotal deaths: {totalDeaths} (In last 24 hours {lastDeaths});\nTested in last 24 hours: {lastTests}, with Antigen tests: {antigenTests}, with PCR tests: {pcrTests};\nquarantined: {quarantine};\nHospitalized: {hospitalize};\nIn Clinic hotels: {clinicHotels}"
 print(text)
