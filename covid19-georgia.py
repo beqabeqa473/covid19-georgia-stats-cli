8a8c07d4e0fe29271e4d7b03e611841a6f7c9aa5
Beqa Gozalishvili <beqaprogger@gmail.com>
2021-02-13 07:01:53 +0400

    adapted to latest website changes

--- covid19-georgia.py
+++ covid19-georgia.py
@@ -17,8 +17,8 @@ lastDeaths = statsItems[2].find_all("span", class_="quantity-numver")[1].text.rs
 lastTests = statsItems[3].find_all("span", class_="quantity-numver")[0].text.rstrip()
 antigenTests = statsItems[3].find_all("span", class_="quantity-numver")[1].text.rstrip()
 pcrTests = statsItems[3].find_all("span", class_="quantity-numver")[2].text.rstrip()
-quarantine = statsItems[4].find_all("span", class_="quantity-numver")[0].text.rstrip()
-hospitalize = statsItems[5].find_all("span", class_="quantity-numver")[0].text.rstrip()
-clinicHotels = statsItems[6].find_all("span", class_="quantity-numver")[0].text.rstrip()
+quarantine = statsItems[5].find_all("span", class_="quantity-numver")[0].text.rstrip()
+hospitalize = statsItems[6].find_all("span", class_="quantity-numver")[0].text.rstrip()
+clinicHotels = statsItems[7].find_all("span", class_="quantity-numver")[0].text.rstrip()
 text = f"total cases: {totalCases} (In last 24 hours {lastCases});\ntotal Recovered: {totalRecovered} (In last 24 hours {lastRecovered});\ntotal deaths: {totalDeaths} (In last 24 hours {lastDeaths});\nTested in last 24 hours: {lastTests}, with Antigen tests: {antigenTests}, with PCR tests: {pcrTests};\nquarantined: {quarantine};\nHospitalized: {hospitalize};\nIn Clinic hotels: {clinicHotels}"
 print(text)
