6948d34e98e7af741173ff64b3f6af3fcdd084c6
Beqa Gozalishvili <beqaprogger@gmail.com>
2021-10-12 07:41:24 +0400

    adapted to current website layout

--- covid19-georgia.py
+++ covid19-georgia.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import re
 from urllib.request import urlopen as req
 
 url = "http://stopcov.ge"
@@ -17,8 +18,11 @@ lastDeaths = statsItems[2].find_all("span", class_="quantity-numver")[1].text.rs
 lastTests = statsItems[3].find_all("span", class_="quantity-numver")[0].text.rstrip()
 antigenTests = statsItems[3].find_all("span", class_="quantity-numver")[1].text.rstrip()
 pcrTests = statsItems[3].find_all("span", class_="quantity-numver")[2].text.rstrip()
-quarantine = statsItems[5].find_all("span", class_="quantity-numver")[0].text.rstrip()
-hospitalize = statsItems[6].find_all("span", class_="quantity-numver")[0].text.rstrip()
-clinicHotels = statsItems[7].find_all("span", class_="quantity-numver")[0].text.rstrip()
-text = f"total cases: {totalCases} (In last 24 hours {lastCases});\ntotal Recovered: {totalRecovered} (In last 24 hours {lastRecovered});\ntotal deaths: {totalDeaths} (In last 24 hours {lastDeaths});\nTested in last 24 hours: {lastTests}, with Antigen tests: {antigenTests}, with PCR tests: {pcrTests};\nquarantined: {quarantine};\nHospitalized: {hospitalize};\nIn Clinic hotels: {clinicHotels}"
+positiveCasesPerDay, positiveCasesPerTwoWeeks, positiveCasesPerWeek = re.findall("\d+,\d+%", statsItems[4].text)
+totalVaccined = statsItems[5].find_all("span", class_="quantity-numver")[0].text.rstrip()
+lastVaccined = statsItems[5].find_all("span", class_="quantity-numver")[1].text.rstrip()
+quarantine = statsItems[6].find_all("span", class_="quantity-numver")[0].text.rstrip()
+hospitalize = statsItems[7].find_all("span", class_="quantity-numver")[0].text.rstrip()
+clinicHotels = statsItems[8].find_all("span", class_="quantity-numver")[0].text.rstrip()
+text = f"total cases: {totalCases} (In last 24 hours {lastCases});\ntotal Recovered: {totalRecovered} (In last 24 hours {lastRecovered});\ntotal deaths: {totalDeaths} (In last 24 hours {lastDeaths});\nTested in last 24 hours: {lastTests}, with Antigen tests: {antigenTests}, with PCR tests: {pcrTests};\nPositive cases: (per day - {positiveCasesPerDay}, per 7 days - {positiveCasesPerWeek}, per 14 days - {positiveCasesPerTwoWeeks});\nVaccined: (total - {totalVaccined}, last day - {lastVaccined});\nquarantined: {quarantine};\nHospitalized: {hospitalize};\nIn Clinic hotels: {clinicHotels}"
 print(text)
